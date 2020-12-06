f6cdfc0a96b0af7b725187ed107f3cd514c12a91
Colin Bell <mailinglist@ps116.org.uk>
2020-12-06 13:18:16 +0000

    Day 6.

--- src/aoc2020/day06.py
+++ src/aoc2020/day06.py
@@ -1,12 +1,55 @@
 import sys
 
 
+def group_score_a(lines):
+    responses = set()
+    for line in lines:
+        for char in line:
+            responses.add(char)
+    return len(responses)
+
+
+def group_score_b(lines):
+    responses = set()
+    for char in lines[0]:
+        responses.add(char)
+
+    for line in lines:
+        r_copy = [r for r in responses]
+        for r in r_copy:
+            if r not in line:
+                responses.remove(r)
+    return len(responses)
+
+
+def group_score(part, lines):
+    if part == 'a':
+        return group_score_a(lines)
+    else:
+        return group_score_b(lines)
+
+
+def both_parts(part, filename):
+    group = []
+    total = 0
+    with open(filename, 'r') as f:
+        for line in f:
+            line = line.rstrip()
+            if line == "":
+                total += group_score(part, group)
+                group = []
+            else:
+                group.append(line)
+    total += group_score(part, group)
+    return total
+
+
 def part_a(filename):
-    return 0
+    return both_parts('a', filename)
 
 
 def part_b(filename):
-    return 0
+    return both_parts('b', filename)
 
 
 def entry():
--- tests/func/test_day06.py
+++ tests/func/test_day06.py
@@ -2,10 +2,8 @@ from aoc2020.day06 import part_a, part_b
 
 
 def test_06a():
-    # assert(part_a('data/day06_test1.txt') == 0)
-    pass
+    assert(part_a('data/day06_test1.txt') == 11)
 
 
 def test_06b():
-    # assert(part_b('data/day06_test1.txt') == 0)
-    pass
+    assert(part_b('data/day06_test1.txt') == 6)
