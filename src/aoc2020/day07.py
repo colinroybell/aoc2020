d7bbcaaef8ced016887c9bf234671ec4edc54c04
Colin Bell <mailinglist@ps116.org.uk>
2020-12-07 17:20:33 +0000

    Day 7.

--- src/aoc2020/day07.py
+++ src/aoc2020/day07.py
@@ -1,12 +1,61 @@
 import sys
 
 
+def parse_file(filename):
+    down = {}
+    up = {}
+    with open(filename, 'r') as f:
+        for line in f:
+            line = line.rstrip()
+            fields = line.split()
+            bag = fields[0] + ' ' + fields[1]
+            pos = 4
+            down[bag] = []
+            while (pos + 3) < len(fields):
+                count = int(fields[pos])
+                subbag = fields[pos+1] + ' ' + fields[pos+2]
+                if bag in down:
+                    down[bag].append((count, subbag))
+                else:
+                    down[bag] = [(count, subbag)]
+                if bag not in up:
+                    up[bag] = []
+                if subbag in up:
+                    up[subbag].append((count, bag))
+                else:
+                    up[subbag] = [(count, bag)]
+                pos += 4
+    return (down, up)
+
+
+def compute_holders(up, bag):
+    holders = set()
+    holders.add(bag)
+    for (count, superbag) in up[bag]:
+        ret = compute_holders(up, superbag)
+        holders = holders.union(ret)
+    return holders
+
+
+# note: caching the answers would be more efficient, but for our
+# case it runs more or less instantly anyway
+def compute_held(down, bag):
+    total = 1
+    for (count, subbag) in down[bag]:
+        total += count * compute_held(down, subbag)
+    return total
+
+
 def part_a(filename):
-    return 0
+    (down, up) = parse_file(filename)
+    holders = compute_holders(up, 'shiny gold')
+    return len(holders) - 1
 
 
 def part_b(filename):
-    return 0
+    (down, up) = parse_file(filename)
+    held = compute_held(down, 'shiny gold')
+    return held - 1
 
 
 def entry():
--- tests/func/test_day07.py
+++ tests/func/test_day07.py
@@ -2,10 +2,9 @@ from aoc2020.day07 import part_a, part_b
 
 
 def test_07a():
-    # assert(part_a('data/day07_test1.txt') == 0)
-    pass
+    assert(part_a('data/day07_test1.txt') == 4)
 
 
 def test_07b():
-    # assert(part_b('data/day07_test1.txt') == 0)
-    pass
+    assert(part_b('data/day07_test1.txt') == 32)
+    assert(part_b('data/day07_test2.txt') == 126)
