d0482a9cb23c1f13932ea7d127add5beea282c4a
Colin Bell <mailinglist@ps116.org.uk>
2020-12-08 08:57:50 +0000

    Day 8.

--- src/aoc2020/day08.py
+++ src/aoc2020/day08.py
@@ -1,11 +1,66 @@
 import sys
 
 
+class State:
+    def __init__(self, program):
+        self.acc = 0
+        self.pos = 0
+        self.program = []
+        print(program)
+        for line in program:
+            line = line.rstrip()
+            fields = line.split()
+            self.program.append((fields[0], int(fields[1])))
+        self.inst_counts = [0] * (len(program) + 1)
+
+    def step(self):
+        self.inst_counts[self.pos] += 1
+        (command, field) = self.program[self.pos]
+        if command == "nop":
+            self.pos += 1
+        if command == "acc":
+            self.acc += field
+            self.pos += 1
+        if command == "jmp":
+            self.pos += field
+
+    def looped(self):
+        return (self.inst_counts[self.pos] > 0)
+
+    def done(self):
+        return (self.pos == len(self.program))
+
+    def get_acc(self):
+        return self.acc
+
+
 def part_a(filename):
-    return 0
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        print(lines)
+    program = State(lines)
+    while not program.looped():
+        program.step()
+    return program.get_acc()
 
 
 def part_b(filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+
+    for i in range(0, len(lines)):
+        cmd = lines[i][0:3]
+        if cmd != 'acc':
+            mod_lines = lines[:]
+            if cmd == 'jmp':
+                mod_lines[i] = 'nop' + mod_lines[i][3:]
+            else:
+                mod_lines[i] = 'jmp' + mod_lines[i][3:]
+            program = State(mod_lines)
+            while not program.looped() and not program.done():
+                program.step()
+            if program.done():
+                return program.get_acc()
     return 0
 
 
