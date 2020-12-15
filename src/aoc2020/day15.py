c3ceca658492d803d3351e228d38508c8b298fc6
Colin Bell <mailinglist@ps116.org.uk>
2020-12-15 05:31:23 +0000

    Day 15.

--- src/aoc2020/day15.py
+++ src/aoc2020/day15.py
@@ -1,12 +1,37 @@
 import sys
 
 
+def run(filename, length):
+    starting = []
+    spoken = []
+    last = {}
+    with open(filename, 'r') as f:
+        line = f.readline()
+        line = line.rstrip()
+        n = line.split(',')
+        for k in n:
+            starting.append(int(k))
+    for r in range(0, length):
+        if r < len(starting):
+            num = starting[r]
+        else:
+            if spoken[-1] in last:
+                num = r - last[spoken[-1]] - 1
+            else:
+                num = 0
+        if r > 0:
+            last[spoken[-1]] = r-1
+        spoken.append(num)
+
+    return spoken[-1]
+
+
 def part_a(filename):
-    return 0
+    return run(filename, 2020)
 
 
 def part_b(filename):
-    return 0
+    return run(filename, 30000000)
 
 
 def entry():
--- tests/func/test_day15.py
+++ tests/func/test_day15.py
@@ -2,10 +2,8 @@ from aoc2020.day15 import part_a, part_b
 
 
 def test_15a():
-    # assert(part_a('data/day15_test1.txt') == 0)
-    pass
+    assert(part_a('data/day15_test1.txt') == 436)
 
 
 def test_15b():
-    # assert(part_b('data/day15_test1.txt') == 0)
-    pass
+    assert(part_b('data/day15_test1.txt') == 175594)
